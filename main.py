a6d25baa9d489056f0f816ebba461f5cfbf91351
mustafabilghn <mustafabilgehanates@hotmail.com>
2025-11-20 21:16:31 +0300

    Add basic ChatBot functionality and JSON knowledge base

---
+++ main.py
@@ -0,0 +1,58 @@
+﻿import json
+from difflib import get_close_matches
+
+
+def load_knowledge_base(file_path: str):
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
+    return data
+
+
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+
+def find_best_match(user_question: str, questions: list[str]) -> str | None:
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
+    return None
+
+
+def chatbot():
+    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
+
+    while True:
+        user_input: str = input("You: ")
+
+        if user_input.lower() == 'quit':
+            break
+
+        # Finds the best match, otherwise returns None
+        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+
+        if best_match:
+            # If there is the best match, return the answer from the knowledge base
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+            print(f"Bot: {answer}")
+        else:
+            print("Bot: I don't know the answer. Can you teach me?")
+            new_answer: str = input("Type the answer or 'skip' to skip: ")
+
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print("Bot: Thank you! I've learned something new.")
+
+
+if __name__ == "__main__":
+    try:
+        chatbot()
+    except KeyboardInterrupt:
+        print("\nChatBot is closing, Goodbye!")

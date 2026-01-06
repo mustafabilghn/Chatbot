7023beff0a4d59e63ccd08edd390eb3179e94ef8
mustafabilghn <mustafabilgehanates@hotmail.com>
2026-01-06 12:55:28 +0300

    Add untracked files and sync project structure

---
+++ ml_bot/intent_responses.py
@@ -0,0 +1,9 @@
+﻿INTENT_RESPONSES = {
+    "selamlama": ["Selam! 😊", "Merhaba kanka 👋", "Hey! Buradayım 😄"],
+    "hal_hatir": ["İyiyim, sen nasılsın?", "Güzel gidiyor, sen?", "Bomba gibiyim!"],
+    "kimlik": ["Ben senin projenim 🤖", "Sohbet etmek için kodlandım 😄"],
+    "smalltalk": ["Anladım 😄", "Hımm, ilginç...", "Devam et bakalım"],
+    "matematik": ["Hesaplamaları severim.", "Matematik benim işim."],
+    "veda": ["Görüşürüz! 👋", "Kendine iyi bak!", "Tekrar beklerim."],
+    "tesekkur": ["Rica ederim! 😊", "Ne demek, her zaman.", "Yardımcı olabildiysem ne mutlu."]
+}
